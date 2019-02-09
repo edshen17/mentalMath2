ea7dbf7ef1eab5a3ac9538cfb965fa5672c63738
Edwin <edshen17@avenues.org>
2019-02-09 16:02:00 +0800

    added routes and cleaned up app.js

--- app.js
+++ app.js
@@ -3,43 +3,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const question = require('./static/question.js');
+const routes = require('./routes/index.js');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/static'));
 app.set('view engine', 'pug');
 
-// include routes
-// var routes = require('./routes/index');
-// app.use('/', routes) ;
-app.get('/', function(req, res, next) {
-  return res.render('index', { title: 'Home' });
-});
+//routes
+app.use('/', routes);
+app.use('/login', routes);
+app.use('/setting', routes);
+app.use('/profile', routes);
+app.use('/play', routes);
 
-app.get('/login', function(req, res, next) {
-  return res.render('index', { title: 'Login' });
-});
-
-app.get('/setting', function(req, res, next) {
-  return res.render('setting', { title: 'Settings' });
-});
-
-app.get('/profile', function(req, res, next) {
-  return res.render('index', { title: 'Profile' });
-});
-
-app.get('/play', function(req, res, next) {
-  const questionArray = question.createQuestions(); //use default values
-  return res.render('play', { title: 'Game', questions: questionArray, score: 0, operation: req.body.radio});
-});
-
-app.post('/play', function(req, res, next) {
-  if (req.body.amount && req.body.min && req.body.max) {
-    const questionArray = question.createQuestions(req.body.radio, req.body.min, req.body.max, req.body.amount);
-    return res.render('play', { questions: questionArray, score: 0, operation: req.body.radio});
-  }
-});
 
 // listen on port 3000
 app.listen(3000, function() {
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,46 @@
-//imports
-// const express = require('express');
-// const router = express.Router();
-//
-// router.get('/', function(req, res, next) {
-//   return res.render('index', { title: 'Home' });
-// });
+'use strict'
+const question = require('../static/question.js');
+const express = require('express');
+const router = express.Router();
+
+// GET /
+// Route for index page
+router.get('/', function(req, res, next) {
+  return res.render('index', { title: 'Home' });
+});
+
+// GET /login
+// Route for login page
+router.get('/login', function(req, res, next) {
+  return res.render('index', { title: 'Login' });
+});
+
+// GET /setting
+// Route for settings page
+router.get('/setting', function(req, res, next) {
+  return res.render('setting', { title: 'Settings' });
+});
+
+// GET /profile
+// Route for profile page
+router.get('/profile', function(req, res, next) {
+  return res.render('index', { title: 'Profile' });
+});
+
+// GET /play
+// Route for game
+router.get('/play', function(req, res, next) {
+  const questionArray = question.createQuestions(); //use default values if none inputted
+  return res.render('play', { title: 'Game', questions: questionArray, score: 0, operation: req.body.radio});
+});
+
+// POST /play
+// Route for creating the game from setting route
+router.post('/play', function(req, res, next) {
+  if (req.body.amount && req.body.min && req.body.max) {
+    const questionArray = question.createQuestions(req.body.radio, req.body.min, req.body.max, req.body.amount);
+    return res.render('play', { questions: questionArray, score: 0, operation: req.body.radio});
+  }
+});
+
+module.exports = router;
